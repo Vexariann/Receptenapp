d1770b04dc47f52718d746a4b4b9bdfb34f94d05
Vexarian <julianpasker@gmail.com>
2024-05-21 10:23:36 +0200

    Initial commit

---
+++ receptenapp/src/main/java/com/julian/receptenapp/ReceptenappApplication.java
@@ -0,0 +1,13 @@
+package com.julian.receptenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptenappApplication.class, args);
+	}
+
+}
---
+++ receptenapp/src/test/java/com/julian/receptenapp/ReceptenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julian.receptenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
